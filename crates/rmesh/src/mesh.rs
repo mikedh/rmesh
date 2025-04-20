db4fa648d35d93ef6f0364fb12e35b4bcd7f7eec
Michael Dawson-Haggerty <mikedh@kerfed.com>
2025-04-20 14:11:57 -0400

    rename macro crate

--- crates/rmesh/src/mesh.rs
+++ crates/rmesh/src/mesh.rs
@@ -8,7 +8,7 @@ use nalgebra::{Point3, Vector3};
 use rayon::prelude::*;
 
 use crate::simplify::simplify_mesh;
-use cache_access::cache_access;
+use rmesh_macro::cache_access;
 
 #[derive(Default, Debug, Clone)]
 struct InnerCache {
