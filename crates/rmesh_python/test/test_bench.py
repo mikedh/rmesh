60bdbee0b45b9da5743223f48113c9d4b48f6d88
Michael Dawson-Haggerty <mikedh@kerfed.com>
2025-05-28 14:38:44 -0400

    print benchmark results in markdown

--- crates/rmesh_python/test/test_bench.py
+++ crates/rmesh_python/test/test_bench.py
@@ -5,8 +5,12 @@ import trimesh
 from io import BytesIO
 from timeit import timeit
 
-# where test models are stored
-_models = os.path.abspath(os.path.expanduser("~/trimesh/models"))
+# current working
+_cwd = os.path.abspath(os.path.expanduser(os.path.dirname(__file__)))
+# root of checkout
+_root = os.path.abspath(os.path.join(_cwd, "..", "..", ".."))
+# if trimesh was cloned next to rmesh
+_models = os.path.abspath(os.path.join(_root, "..", "trimesh", "models"))
 
 
 def compare(file_data: bytes, file_type: str):
@@ -20,13 +24,13 @@ def compare(file_data: bytes, file_type: str):
 
     count = 1
     tri = timeit(
-        stmt="trimesh.load_mesh(file_obj=BytesIO(file_data), file_type=file_type)",
+        stmt="len(trimesh.load_mesh(file_obj=BytesIO(file_data), file_type=file_type).vertices)",
         setup="import trimesh; from io import BytesIO",
         number=count,
         globals={"file_data": file_data, "file_type": file_type},
     )
     rme = timeit(
-        stmt="rmesh.load_mesh(file_data=file_data, file_type=file_type)",
+        stmt="len(rmesh.load_mesh(file_data=file_data, file_type=file_type).vertices)",
         setup="import rmesh",
         number=count,
         globals={"file_data": file_data, "file_type": file_type},
@@ -35,15 +39,40 @@ def compare(file_data: bytes, file_type: str):
     return {"trimesh": tri / count, "rmesh": rme / count, "ratio": tri / rme}
 
 
+def to_markdown(results: dict) -> str:
+    """ """
+    mark = [
+        "| File Name | `trimesh` | `rmesh` | Speedup |",
+        "|    --     |    --     |   --    |   --    |",
+    ]
+    mark.extend(
+        f"| {k} | {r['trimesh']:0.4f}s | {r['rmesh']:0.4f}s | {r['ratio']:0.2f}x |"
+        for k, r in results.items()
+    )
+
+    return "\n".join(mark)
+
+
 def test_both():
+    # if we didn't check out trimesh for the models corpus
+    if not os.path.exists(_models):
+        print(f"models not in expected location (`{_models}`), exiting!")
+        return
+
     results = {}
     for file_name in os.listdir(_models):
-        with open(os.path.join(_models, file_name), "rb") as f:
+        file_path = os.path.join(_models, file_name)
+        if not os.path.isfile(file_path):
+            continue
+        with open(file_path, "rb") as f:
             file_data = f.read()
         file_type = trimesh.util.split_extension(file_name)
 
-        results[file_name] = compare(file_data=file_data, file_type=file_type)
-        print(file_name, results[file_name])
+        if result := compare(file_data=file_data, file_type=file_type):
+            results[file_name] = result
+            print(file_name, result)
+
+    print(to_markdown(results))
 
 
 if __name__ == "__main__":
